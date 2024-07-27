527ede9ab48a90e9b93b33a8634cfcdc6952b89c
SOJAN <sojanonelson54@gmail.com>
2024-07-27 17:33:53 +0530

    changes

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -6,10 +6,10 @@ const Banner = () => {
     <div className="relative">
       <img src={Image} alt="banner" className="w-full h-[40%]" />
       <div className="absolute top-4  lg:top-7 lg:left-[900px] left-52   h-full flex flex-col items-center justify-center text-white">
-        <h1 className="lg:text-6xl text-xl poppins-bold font-bold">Explore निरन्तरा...</h1>
-        <h1 className='text-white lg:text-xl poppins-thin'>EXPLORE WITH US</h1>
-        <div className="lg:mt-3 lg:p-2 cursor-pointer hover:bg-[#1D8E8E] text-[#1D8E8E]  hover:text-white bg-white rounded-sm lg:rounded-lg">
-          <h1 className="lg:text-lg text-sm ">Plan Your Trip</h1>
+        <h1 className="lg:text-6xl  text-xl poppins-bold font-bold">Explore निरन्तरा...</h1>
+        <h1 className='text-white lg:text-xl poppins-regular'>EXPLORE WITH US</h1>
+        <div className="lg:mt-3 lg:p-2 cursor-pointer rounded-lg hover:bg-white hover:text-[#1D8E8E]   text-white bg-[#1D8E8E]  lg:rounded-lg">
+          <h1 className="lg:text-lg text-sm poppins-bold">Plan Your Trip</h1>
         </div>
       </div>
     </div>
